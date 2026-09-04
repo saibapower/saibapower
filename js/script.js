46190d60eddc2d032a0a8e91bf764f0cd516ec4e
saibapower <78josephmaina73@gmail.com>
2026-09-04 19:06:14 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const revealObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+});
 
+document.querySelectorAll('.reveal').forEach((element) => {
+    revealObserver.observe(element);
+});
